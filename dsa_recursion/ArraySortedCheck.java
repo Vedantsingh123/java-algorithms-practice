7b5822453adef6bdabf8f4cf587cdd7d29face2a
vedant <vedant9672@gmail.com>
2025-08-04 23:22:25 +0530

    Added ArraySorted.java in dsa_recursion

---
+++ dsa_recursion/ArraySortedCheck.java
@@ -0,0 +1,16 @@
+public class ArraySortedCheck {
+    
+    static boolean sorted(int[] arr, int i){
+        if(i == arr.length-1){
+            return true;
+        }
+        if(arr[i] > arr[i+1]){
+            return false;
+        }
+        return sorted(arr, i+1);
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 9, 8};
+        System.out.println(sorted(arr, 0));
+    }
+}
